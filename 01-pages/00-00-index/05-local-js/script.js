0cf6ad5fd7fec10de8fff6b19ed5d7dd6c0d5f09
iveroh <142319965+iveroh@users.noreply.github.com>
2024-11-12 11:11:07 +0100

    feat(00-00-index): Added navbar with sticky properties - Further improvement of 00-00-index style and functionality - Added navbar with sticky properties

--- 01-pages/00-00-index/05-local-js/script.js
+++ 01-pages/00-00-index/05-local-js/script.js
@@ -1,3 +1,9 @@
+/*
+/*
+/* JavaScript file for 00-00-index
+/*
+*/
+
 //opening index.html fade
 window.onload = function () {
   const whiteBox = document.querySelector(".frontpage-fader");
@@ -5,3 +11,18 @@ window.onload = function () {
     whiteBox.style.opacity = "0";
   }, 1500);
 };
+
+//Sticks navbar
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.querySelector(".navbar");
+  const topContent = document.querySelector(".top-content");
+
+  window.addEventListener("scroll", function () {
+      // Check if the scroll position is past the top content section
+      if (window.scrollY >= topContent.offsetHeight) {
+          navbar.classList.add("sticky"); // Make navbar sticky
+      } else {
+          navbar.classList.remove("sticky"); // Remove sticky behavior when above top content
+      }
+  });
+});
