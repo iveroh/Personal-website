6a3d5d152da7a07233a274792f46a7ab321c3d1b
iveroh <142319965+iveroh@users.noreply.github.com>
2024-11-16 16:15:38 +0100

    feat(11-resources): Added favicon - Added favicon in resources to be used in all html files. - favicon file must be updated for better quality

--- 11-resources/05-js/common-script.js
+++ 11-resources/05-js/common-script.js
@@ -1,21 +1,12 @@
-/**
- *
- * STANDARD: JavaScript file, creates standard navbar and footer in all elements
- * that apply the script
- *
- */
+//Creates the nav and footer in all elements
 
-/**
- *
- * Lists to be used in createLinks() function
- *
- */
-
+//lists to make links to navigate the pages.
+//0: class, 1: href, 2: html, 3; target
 let navList = [
-  ["home", "index.html", "Iver Heggelund | IOH", ""],
-  ["article", "navigation.html", "Articles | IOH", ""],
-  ["about", "about.html", "About | IOH", ""],
-  ["contact", "contact.html", "Contact | IOH", ""],
+  ["home", "/01-pages/00-00-index/index.html", "HOME", ""],
+  ["article", "navigation.html", "ARTICLES", ""],
+  ["about", "about.html", "ABOUT", ""],
+  ["contact-us", "contact.html", "Contact us", ""],
 ];
 
 let socialList = [
@@ -25,41 +16,34 @@ let socialList = [
   ["discord logo", "https://www.discord.com/", "", "_blank"],
 ];
 
-/**
- * Takes in a list written like those above and where to append the html elements created
- *
- * @param {*} list
- * @param {*} place
- */
-
+//Reusible function that makes html code for links.
+//Is used 3 times to create all the links in nav and footer
+//Takes in a list written like those above and where to append the html elements created
 function createLinks(list, place) {
+  //creating articles
   for (let el in list) {
     const klasse = list[el][0];
     const link = list[el][1];
     const name = list[el][2];
     const target = list[el][3];
     place.innerHTML += `   
-          <a href="${link}" class="${klasse}" target="${target}">${name}</a>`;
+        <a href="${link}" class="${klasse}" target="${target}">${name}</a>`;
   }
 }
 
-/**
- *
- * NAVBAR: Creates navbar
- *
- */
+//creating navbar
 function navbar() {
   //collecting elements from DOM
   const header = document.querySelector("header");
   const navbar = document.createElement("nav");
 
-  header.innerHTML +=
-    '<img src="img/logos/navbar.svg" class="pulldown-button" alt="pulldown menu">';
+  //Img for navbar and toggle class for mobile version
+  header.innerHTML += '<img src="img/logos/navbar.svg" class="pulldown-button" alt="pulldown menu">';
   const pulldown = document.querySelector(".pulldown-button");
   pulldown.addEventListener("click", () => {
     navbar.classList.toggle("toggle");
     header.classList.toggle("toggle-header");
-  });
+  })
 
   //appending navbar
   header.append(navbar);
@@ -67,3 +51,56 @@ function navbar() {
   //creating links
   createLinks(navList, navbar);
 }
+
+//Function for footer
+function footer() {
+  //getting footer from Dom
+  const footer = document.querySelector("footer");
+
+  //DIV FOR CONTACT INFO (left)
+  const contactDiv = document.createElement("div");
+  contactDiv.classList = "contact-div";
+  footer.append(contactDiv);
+
+  contactDiv.innerHTML += `
+  <h3>Contact information</h3>
+  <p>Epost:</p>
+  <p>studyHow@wedongivashit.no</p>
+  <p>Tlf</p>
+  <p>605-475-6961</p>
+  <p>Adress:</p>
+  <a href="https://maps.app.goo.gl/LzLRzUTYwhejVio26" target="_blank">Slottsplassen 1, 0010 Oslo</a>
+  `;
+
+  //DIV FOR SOCIAL LINKS AND LOGO (center)
+  //making to divs to add links and logos
+  const centerDiv = document.createElement("div");
+  centerDiv.classList = "center-div";
+  footer.append(centerDiv);
+
+  const socialDiv = document.createElement("div");
+  socialDiv.classList = "social-div";
+  centerDiv.append(socialDiv);
+
+  //create social links
+  createLinks(socialList, socialDiv);
+
+  //adding logo
+  centerDiv.innerHTML += `
+  <img src="img/anlain_logo.svg" alt="Logo of Ånlain">
+  <p>&copy;gruppe 4 2023</p>`;
+  
+  //DIV FOR NAVIGATION (right)
+  const navDiv = document.createElement("div");
+  navDiv.classList = "navigation-div";
+  footer.append(navDiv);
+
+  //create navigation links
+  createLinks(navList, navDiv);
+
+  //Link to sitemap
+  navDiv.innerHTML += '<a href="sitemap.html" class="sitemap">Sitemap</a>'
+}
+//Calling the function
+footer();
+navbar();
