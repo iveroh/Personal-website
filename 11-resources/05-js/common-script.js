9d79b3a2d86726d3f64911f1ea44ecf440b9bace
iveroh <142319965+iveroh@users.noreply.github.com>
2024-11-18 03:39:43 +0100

    fix(root): Moved index.html to root

--- 11-resources/05-js/common-script.js
+++ 11-resources/05-js/common-script.js
@@ -13,7 +13,7 @@ function createNavbar() {
 
   // Define the navigation items
   const navItems = [
-    { text: "HOME", href: "/01-pages/00-00-index/index.html" },
+    { text: "HOME", href: "index.html" },
     { text: "ABOUT", href: "/01-pages/01-00-about/about.html" },
     { text: "ARTICLES", href: "/01-pages/02-00-articles/navigation.html" },
     { text: "CONTACT", href: "/01-pages/03-00-contact/contact.html" },
